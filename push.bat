19fd36b66badd2f5b64f635efaca179a20c768bf
muggle <isocket@outlook.com>
2022-04-24 20:00:24 +0800

    Site updated: 2022-04-24 20:00:22

--- push.bat
+++ push.bat
@@ -1,11 +1,1 @@
-
-echo hexo g
-hexo g
-echo hexo d
-hexo d
-echo git add .
-git add .
-echo git commit 
-git commit -m"xxx"
-echo git push
-git push
+hexo g & hexo d & git add . & git commit -m"xxx" & git push
